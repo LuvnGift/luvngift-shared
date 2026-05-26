adb8ca115cdf3f57709185d2934b9950525c9c94
Oluwatobi Onipede <onipede89@gmail.com>
2026-05-25 23:12:55 -0400

    add some MVP code

--- src/types/vendor.types.ts
+++ src/types/vendor.types.ts
@@ -1,9 +1,14 @@
+export type VendorBusinessType = 'RETAIL' | 'DELIVERY' | 'LOGISTICS';
+
 export interface Vendor {
   id: string;
   name: string;
   email: string;
   phone: string;
   address: string;
+  state: string;
+  businessType: VendorBusinessType;
+  notes?: string | null;
   isActive: boolean;
   createdAt: Date;
   updatedAt: Date;
